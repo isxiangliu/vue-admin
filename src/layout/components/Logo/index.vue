b8e0c5e532b7bfe2a020c1e1cb464345521185f4
liuxiang <xiangliu8116@loongair.cn>
2023-02-28 14:35:09 +0800

    新模式路由架构

--- src/layout/components/Logo/index.vue
+++ src/layout/components/Logo/index.vue
@@ -1,45 +1,73 @@
 <template>
-    <router-link v-slot="{ navigate }" custom :to="to" :class="{
-        'title': true,
-        'is-link': $store.state.settings.enableDashboard
-    }" :title="title" tag="div"
+  <div class="nav-tar">
+    <router-link
+      v-slot="{ navigate }"
+      custom
+      :to="to"
+      :class="{
+        title: true,
+        'is-link': $store.state.settings.enableDashboard,
+      }"
+      :title="title"
+      tag="div"
     >
-        <div @click="navigate">
-            <img v-if="showLogo" :src="logo" class="logo">
-            <span v-if="showTitle">{{ title }}</span>
-        </div>
+      <div @click="navigate">
+        <img v-if="showLogo" :src="logo" class="logo" />
+        <span v-if="showTitle">{{ title }}</span>
+      </div>
     </router-link>
+    <div
+      :class="['nav-title', { active: currentIndex == index }]"
+      @click="titleClick(index)"
+      v-for="(item, index) in titleBar"
+      :key="item.id"
+    >
+      <span>{{ item.title }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Logo',
-    props: {
-        showLogo: {
-            type: Boolean,
-            default: true
-        },
-        showTitle: {
-            type: Boolean,
-            default: true
-        }
+  name: 'Logo',
+  props: {
+    showLogo: {
+      type: Boolean,
+      default: true,
     },
-    data() {
-        return {
-            title: process.env.VUE_APP_TITLE,
-            logo: require('@/assets/images/min-logo.png')
-        }
+    showTitle: {
+      type: Boolean,
+      default: true,
     },
-    computed: {
-        to() {
-            let rtn = {}
-            if (this.$store.state.settings.enableDashboard) {
-                rtn.name = 'dashboard'
-            }
-            return rtn
-        }
-    }
-}
+  },
+  data() {
+    return {
+      title: process.env.VUE_APP_TITLE,
+      logo: require('@/assets/images/min-logo.png'),
+      titleBar: [
+        { id: 0, title: '首页' },
+        { id: 1, title: '组织管理' },
+        { id: 2, title: '招聘管理' },
+        { id: 3, title: '人事管理' },
+      ],
+      currentIndex: 0,
+    };
+  },
+  computed: {
+    to() {
+      let rtn = {};
+      if (this.$store.state.settings.enableDashboard) {
+        rtn.name = 'dashboard';
+      }
+      return rtn;
+    },
+  },
+  methods: {
+    titleClick(index) {
+      this.currentIndex = index;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -56,6 +84,7 @@ export default {
     text-align: center;
     overflow: hidden;
     text-decoration: none;
+    margin-right: 40px;
     &.is-link {
         cursor: pointer;
     }
@@ -71,6 +100,21 @@ export default {
         font-weight: bold;
         color: #fff;
         @include text-overflow;
+    }
+}
+.nav-tar {
+    display: flex;
+    .nav-title {
+        text-align: center;
+        padding: 8px 0;
+        line-height: 50px;
+        cursor: pointer;
+        span {
+            padding: 0 12px;
+        }
+        &.active {
+            border-bottom: 3px solid red;
+        }
     }
 }
 </style>
