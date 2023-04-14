cb53138cd5de57d621daf2651f0c91d3187c3433
liuxiang <xiangliu8116@loongair.cn>
2023-04-14 16:40:54 +0800

    three模型的应用

--- src/views/multilevel_menu_example/page.vue
+++ src/views/multilevel_menu_example/page.vue
@@ -8,6 +8,9 @@
                 :style="{ width: screenWidth - 110 + 'px', height: screenHeight - 110 + 'px' }"
             />
         </el-dialog>
+        <PageMain>
+            <canvas id="three" />
+        </PageMain>
         <PageMain class="viewNav">
             <el-row :gutter="10">
                 <el-col :span="12">
@@ -144,6 +147,9 @@ import HorizontalBarChart from './components/HorizontalBarChart'
 import BlockTitle from './components/BlockTitle'
 import AllAgeEmployees from './components/AllAgeEmployees'
 import Chart from '@/mixin/chart'
+import * as THREE from 'three/build/three.module'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 export default {
     components: {
         ExampleNotice,
@@ -192,7 +198,9 @@ export default {
             this.screenWidth = val
         }
     },
-    created() {},
+    created() {
+        
+    },
     mounted() {
         // const option = {
         //   content: '测试水印',
@@ -209,8 +217,105 @@ export default {
                 this.screenWidth = window.innerWidth
             })()
         }
+        this.init()
     },
     methods: {
+        init() {
+            const scene = new THREE.Scene()
+            scene.background = new THREE.Color('#eee')
+            // 给场景加个雾
+            scene.fog = new THREE.Fog('#eee', 20, 100)
+            const canvas = document.querySelector('#three')
+            const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
+            // 添加影子
+            renderer.shadowMap.enabled = true
+            const camera = new THREE.PerspectiveCamera(
+                50,
+                window.innerWidth / window.innerHeight,
+                0.1,
+                1000
+            )
+            camera.position.set(0, 0, 10)
+            
+            const controls = new OrbitControls(camera, renderer.domElement)
+            controls.enableDamping = true// 添加阻尼感 真实
+            function animate() {
+                renderer.render(scene, camera)
+                controls.update()
+                requestAnimationFrame(animate)
+                if (resizeRendererToDisplaySize(renderer)) {
+                    const canvas = renderer.domElement
+                    camera.aspect = canvas.clientWidth / canvas.clientHeight
+                    camera.updateProjectionMatrix()
+                }
+            }
+            animate()
+
+            const gltfLoader = new GLTFLoader()
+            gltfLoader.load('/seraphine/scene.gltf', gltf => {
+                let model = gltf.scene
+                // 遍历模型每部分
+                model.traverse(i => {
+                    // 将图片作为纹理加载
+                    let explosionTexture = new THREE.TextureLoader().load(
+                        '/seraphine/textures/Mat_cwfyfr1_userboy17.bmp_diffuse.png'
+                    )
+                    // 调整纹理图的方向
+                    explosionTexture.flipY = false
+                    // 将纹理图生成基础网格材质(MeshBasicMaterial)
+                    const material = new THREE.MeshBasicMaterial({
+                        map: explosionTexture
+                    })
+                    // 给模型每部分上材质
+                    i.material = material
+
+                    // 让模型等每个部分都能产生阴影
+                    if (i.isMesh) {
+                        i.castShadow = true
+                        i.receiveShadow = true
+                    }
+                })
+                scene.add(model)
+            })
+            
+            // 此属性返回当前显示设备的物理像素分辨率与CSS像素分辨率的比值 解决模糊
+            function resizeRendererToDisplaySize(renderer) {
+                const canvas = renderer.domElement
+                var width = window.innerWidth
+                var height = window.innerHeight * 2 - 200
+                var canvasPixelWidth = canvas.width / window.devicePixelRatio
+                var canvasPixelHeight = canvas.height / window.devicePixelRatio
+                const needResize = canvasPixelWidth !== width || canvasPixelHeight !== height
+                if (needResize) {
+                    renderer.setSize(width, height, false)
+                }
+                return needResize
+            }
+
+            // 添加光与影
+            let floorGeometry = new THREE.PlaneGeometry(3000, 3000) // 平面几何体
+            let floorMaterial = new THREE.MeshPhongMaterial({color: 0x857ebb, shininess: 0})
+            let floor = new THREE.Mesh(floorGeometry, floorMaterial)
+            floor.rotation.x = -0.5 * Math.PI
+            floor.receiveShadow = true
+            floor.position.y = -0.001
+            scene.add(floor)
+            
+            // 平行光
+            const dirLight = new THREE.DirectionalLight(0xffffff, 0.6)
+            // 光源等位置
+            dirLight.position.set(-10, 8, -5)
+            // 可以产生阴影
+            dirLight.castShadow = true
+            dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024)
+            scene.add(dirLight)
+            
+            // 添加多个光源，让场景看起来更真实
+            const hemLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6)
+            hemLight.position.set(0, 48, 0)
+            scene.add(hemLight)
+        },
+
         getList(val) {
             console.log('表格数据更新')
             const blob = new Blob(['htmlCode!'], { type: '' })
@@ -338,6 +443,10 @@ export default {
 }
 .viewNav {
     background-color: #00031b;
+}
+#three {
+    width: 100%;
+    height: 100%;
 }
 </style>
 
